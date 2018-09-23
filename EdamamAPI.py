174c1e933f8b2f284f130a10d0a04a667997f2d4
Neil Rustagi <neilrustagi@gmail.com>
2018-09-22 22:12:48 -0700

    Need to test for a food to make sure other nutrients are actually workable. also need to add the knowledge api

--- EdamamAPI.py
+++ EdamamAPI.py
@@ -29,78 +29,97 @@ CARBS = ('There are ' + str(nutrientdata['CHOCDF']) + " grams of carbohydrates i
 CARBS = CARBS.lower().capitalize()
 print(CARBS)
 
-CLCM = ('There are ' + str(nutrientdata['CA']) + " milligrams of Calcium in " + str(food) + ".")
-CLCM = CLCM.lower().capitalize()
-print(CLCM)
+if 'CA' in nutrientdata:
+    CLCM = ('There are ' + str(nutrientdata['CA']) + " milligrams of Calcium in " + str(food) + ".")
+    CLCM = CLCM.lower().capitalize()
+    print(CLCM)
 
-CLSTRL = ('There are ' + str(nutrientdata['CHOLE']) + " milligrams of cholestrol in " + str(food) + ".")
-CLSTRL = CLSTRL.lower().capitalize()
-print(CLSTRL)
+if 'CHOLE' in nutrientdata:
+    CLSTRL = ('There are ' + str(nutrientdata['CHOLE']) + " milligrams of cholestrol in " + str(food) + ".")
+    CLSTRL = CLSTRL.lower().capitalize()
+    print(CLSTRL)
 
-MONOFAT = ('There are ' + str(nutrientdata['FAMS']) + " grams of Monounsaturated Fat in " + str(food) + ".")
-MONOFAT = MONOFAT.lower().capitalize()
-print(MONOFAT)
+if 'MONOFAT' in nutrientdata:
+    MONOFAT = ('There are ' + str(nutrientdata['FAMS']) + " grams of Monounsaturated Fat in " + str(food) + ".")
+    MONOFAT = MONOFAT.lower().capitalize()
+    print(MONOFAT)
 
-POLYFAT = ('There are ' + str(nutrientdata['FAPU']) + " grams of Polyunsaturated Fat in " + str(food) + ".")
-POLYFAT = POLYFAT.lower().capitalize()
-print(POLYFAT)
+if 'POLYFAT' in nutrientdata:
+    POLYFAT = ('There are ' + str(nutrientdata['FAPU']) + " grams of Polyunsaturated Fat in " + str(food) + ".")
+    POLYFAT = POLYFAT.lower().capitalize()
+    print(POLYFAT)
 
-SATFAT = ('There are ' + str(nutrientdata['FASAT']) + " grams of Saturated Fat in " + str(food) + ".")
-SATFAT = SATFAT.lower().capitalize()
-print(SATFAT)
+if 'SATFAT' in nutrientdata:
+    SATFAT = ('There are ' + str(nutrientdata['FASAT']) + " grams of Saturated Fat in " + str(food) + ".")
+    SATFAT = SATFAT.lower().capitalize()
+    print(SATFAT)
 
-TRANFAT = ('There are ' + str(nutrientdata['FATRN']) + " grams of Trans Fat in " + str(food) + ".")
-TRANFAT = TRANFAT.lower().capitalize()
-print(TRANFAT)
+if 'MONOFAT' in nutrientdata:
+    TRANFAT = ('There are ' + str(nutrientdata['FATRN']) + " grams of Trans Fat in " + str(food) + ".")
+    TRANFAT = TRANFAT.lower().capitalize()
+    print(TRANFAT)
 
-IRN = ('There are ' + str(nutrientdata['FE']) + " milligrams of iron in " + str(food) + ".")
-IRN = IRN.lower().capitalize()
-print(IRN)
+if 'IRN' in nutrientdata:
+    IRN = ('There are ' + str(nutrientdata['FE']) + " milligrams of iron in " + str(food) + ".")
+    IRN = IRN.lower().capitalize()
+    print(IRN)
 
-FBR = ('There are ' + str(nutrientdata['FIBTG']) + " grams of fiber in " + str(food) + ".")
-FBR = FBR.lower().capitalize()
-print(FBR)
+if 'FBR' in nutrientdata:
+    FBR = ('There are ' + str(nutrientdata['FIBTG']) + " grams of fiber in " + str(food) + ".")
+    FBR = FBR.lower().capitalize()
+    print(FBR)
 
-PTSM = ('There are ' + str(nutrientdata['K']) + " milligrams of potassium in " + str(food) + ".")
-PTSM = PTSM.lower().capitalize()
-print(PTSM)
+if 'PTSM' in nutrientdata:
+    PTSM = ('There are ' + str(nutrientdata['K']) + " milligrams of potassium in " + str(food) + ".")
+    PTSM = PTSM.lower().capitalize()
+    print(PTSM)
 
-MGNSM = ('There are ' + str(nutrientdata['MG']) + " milligrams of magnesium in " + str(food) + ".")
-MGNSM = MGNSM.lower().capitalize()
-print(MGNSM)
 
-SDM = ('There are ' + str(nutrientdata['NA']) + " milligrams of sodium in " + str(food) + ".")
-SDM = SDM.lower().capitalize()
-print(SDM)
+    MGNSM = ('There are ' + str(nutrientdata['MG']) + " milligrams of magnesium in " + str(food) + ".")
+    MGNSM = MGNSM.lower().capitalize()
+    print(MGNSM)
 
-SGR = ('There are ' + str(nutrientdata['SUGAR']) + " grams of sugar in " + str(food) + ".")
-SGR = SGR.lower().capitalize()
-print(SGR)
 
-VitE = ('There are ' + str(nutrientdata['TOCPHA']) + " milligrams of Vitamin E in " + str(food) + ".")
-VitE = VitE.lower().capitalize()
-print(VitE)
+    SDM = ('There are ' + str(nutrientdata['NA']) + " milligrams of sodium in " + str(food) + ".")
+    SDM = SDM.lower().capitalize()
+    print(SDM)
 
-VitA = ('There are ' + str(nutrientdata['VITARAE']) + " æ-grams of Vitamin A in " + str(food) + ".")
-VitA = VitA.lower().capitalize()
-print(VitA)
 
-VitB12 = ('There are ' + str(nutrientdata['VITB12']) + " æ-grams of Vitamin B12 in " + str(food) + ".")
-VitB12 = VitB12.lower().capitalize()
-print(VitB12)
+    SGR = ('There are ' + str(nutrientdata['SUGAR']) + " grams of sugar in " + str(food) + ".")
+    SGR = SGR.lower().capitalize()
+    print(SGR)
 
-VitB6 = ('There are ' + str(nutrientdata['VITB6']) + " milligrams of Vitamin B6 in " + str(food) + ".")
-VitB6 = VitB6.lower().capitalize()
-print(VitB6)
 
-VitC = ('There are ' + str(nutrientdata['VITC']) + " milligrams of Vitamin C in " + str(food) + ".")
-VitC = VitC.lower().capitalize()
-print(VitC)
+    VitE = ('There are ' + str(nutrientdata['TOCPHA']) + " milligrams of Vitamin E in " + str(food) + ".")
+    VitE = VitE.lower().capitalize()
+    print(VitE)
 
-VitD = ('There are ' + str(nutrientdata['VITD']) + " æ-grams of Vitamin D in " + str(food) + ".")
-VitD = VitD.lower().capitalize()
-print(VitD)
 
-VitK = ('There are ' + str(nutrientdata['VITK1']) + " æ-grams of Vitamin K in " + str(food) + ".")
-VitK = VitK.lower().capitalize()
-print(VitK)
+    VitA = ('There are ' + str(nutrientdata['VITARAE']) + " æ-grams of Vitamin A in " + str(food) + ".")
+    VitA = VitA.lower().capitalize()
+    print(VitA)
+
+
+    VitB12 = ('There are ' + str(nutrientdata['VITB12']) + " æ-grams of Vitamin B12 in " + str(food) + ".")
+    VitB12 = VitB12.lower().capitalize()
+    print(VitB12)
+
+
+    VitB6 = ('There are ' + str(nutrientdata['VITB6']) + " milligrams of Vitamin B6 in " + str(food) + ".")
+    VitB6 = VitB6.lower().capitalize()
+    print(VitB6)
+
+
+    VitC = ('There are ' + str(nutrientdata['VITC']) + " milligrams of Vitamin C in " + str(food) + ".")
+    VitC = VitC.lower().capitalize()
+    print(VitC)
+
+
+    VitD = ('There are ' + str(nutrientdata['VITD']) + " æ-grams of Vitamin D in " + str(food) + ".")
+    VitD = VitD.lower().capitalize()
+    print(VitD)
+
+
+    VitK = ('There are ' + str(nutrientdata['VITK1']) + " æ-grams of Vitamin K in " + str(food) + ".")
+    VitK = VitK.lower().capitalize()
+    print(VitK)
