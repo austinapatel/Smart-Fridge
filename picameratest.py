c306ef41a768e2548f4cd7cd43cda5fe330c70ba
Austin Patel <austinapatel@gmail.com>
2018-09-22 22:09:09 -0700

    udpate camera

--- picameratest.py
+++ picameratest.py
@@ -9,12 +9,13 @@ from aiy.pins import PIN_D
 camera = PiCamera()
 
 # Set up a gpiozero Button using the 4th pin on the vision hat expansion.
-button = Button(PIN_D)
+# button = Button(PIN_D)
 
 # When the button is pressed, call the led.on() function (turn the led on)
-button.when_pressed = camera.start_preview
+camera.start_preview()
+sleep(10)
 # When the button is released, call the led.off() function (turn the led off)
-button.when_released = camera.stop_preview
+camera.stop_preview()
 
 # Wait for the user to kill the example.
 pause()
