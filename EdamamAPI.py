7491ab26b032f6b84e5ed410fe2583647fd9aedb
Neil Rustagi <neilrustagi@gmail.com>
2018-09-22 17:18:51 -0700

    Funcitonal apple nutritional data. Working on printing all data, and testing how many foods are usable in the program, and cleanup in the prints.

--- EdamamAPI.py
+++ EdamamAPI.py
@@ -1,8 +1,14 @@
-import requests
+import requests, json
 
-apikey = "01cf97dd"
-appid = "b920660a822e1fddc5b10bdf50c890cf"
-test = "https://api.edamam.com/api/food-database/parser?ingr=apple&app_id=b920660a822e1fddc5b10bdf50c890cf&app_key=01cf97dd"
+appid = "01cf97dd"
+apikey = "b920660a822e1fddc5b10bdf50c890cf"
+test = "https://api.edamam.com/api/food-database/parser?ingr=apple&app_id=01cf97dd&app_key=b920660a822e1fddc5b10bdf50c890cf"
 
 response = requests.get("https://api.edamam.com/api/food-database/parser?ingr={}&app_id={}&app_key={}".format("apple", appid, apikey))
-print(response.content)
+#print(response.content.decode("utf-8"))
+
+fooddata = json.loads(response.content.decode("utf-8"))
+nutrientdata = fooddata["parsed"][0]["food"]["nutrients"]
+food = fooddata["parsed"][0]["food"]["label"]
+
+print('There are', nutrientdata['ENERC_KCAL'], "calories in", food)
