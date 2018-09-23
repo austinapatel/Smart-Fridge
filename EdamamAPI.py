599564369286fd18e55c62503b6c37a11dd546b4
Neil Rustagi <neilrustagi@gmail.com>
2018-09-22 17:48:52 -0700

    changed decoding to not print but still decode

--- EdamamAPI.py
+++ EdamamAPI.py
@@ -5,7 +5,7 @@ apikey = "b920660a822e1fddc5b10bdf50c890cf"
 test = "https://api.edamam.com/api/food-database/parser?ingr=apple&app_id=01cf97dd&app_key=b920660a822e1fddc5b10bdf50c890cf"
 
 response = requests.get("https://api.edamam.com/api/food-database/parser?ingr={}&app_id={}&app_key={}".format("apple", appid, apikey))
-#print(response.content.decode("utf-8"))
+(response.content.decode("utf-8"))
 
 fooddata = json.loads(response.content.decode("utf-8"))
 nutrientdata = fooddata["parsed"][0]["food"]["nutrients"]
