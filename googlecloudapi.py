47e4d66361a7b3095a29c679d697af334adf671b
ILikeDankMemes <noaharias@outlook.com>
2018-09-22 17:30:45 -0700

    Noah's Files

--- googlecloudapi.py
+++ googlecloudapi.py
@@ -9,7 +9,7 @@ from google.cloud.vision import types
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
-file_name = os.path.join(os.path.dirname(__file__),'resources/wakeupcat.jpg')
+file_name = os.path.join(os.path.dirname(__file__),'')
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
