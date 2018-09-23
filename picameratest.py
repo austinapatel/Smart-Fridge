424374dac5f6921c8874b560119d5beaeedea21f
Austin Patel <austinapatel@gmail.com>
2018-09-23 03:16:44 -0700

    fix path for saving cropped images

--- picameratest.py
+++ picameratest.py
@@ -52,7 +52,7 @@ def crop(image_path, coords, saved_location):
     """
     image_obj = Image.open(image_path)
     cropped_image = image_obj.crop(coords)
-    cropped_image.save(saved_location)
+    cropped_image.save(base + saved_location)
     cropped_image.show()
 
 
